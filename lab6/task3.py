712274c18bfe4c54bb3901861695bb941396cc13
Taniriya <137798683+Taniriya@users.noreply.github.com>
2025-05-30 22:54:29 +0300

    Update task3.py

--- lab6/task3.py
+++ lab6/task3.py
@@ -7,5 +7,5 @@ data = pd.read_csv('students_with_gaps.csv', sep=',')
 print(data.isnull().sum())
 # Заполняем пропуски в столбце Score средним значением
 new_data = data.fillna(value={'Score': data['Score'].mean()})
-# Удаляем строки с пропусками в столбце Group
+# Удаляем строки с пропусками в остальных столбцах
 newest_data = new_data.dropna()
