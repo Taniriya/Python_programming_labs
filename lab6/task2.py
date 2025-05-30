5afd00a1042a34cdcf2c00a349b893c1e3031d76
Taniriya <137798683+Taniriya@users.noreply.github.com>
2025-05-30 22:55:16 +0300

    Update task2.py

--- lab6/task2.py
+++ lab6/task2.py
@@ -7,5 +7,7 @@ data = pd.read_csv('students.csv', sep=',')
 print(data[data['Score'] > 80])
 # Сортируем студентов по убыванию балла
 print(data.sort_values(by='Score', ascending=False))
+# Выводим студентов с максимальным возрастом
 print(data[data['Age'] == data['Age'].max()])
+# И минимальным
 print(data[data['Age'] == data['Age'].min()])
